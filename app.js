56a0efd6e523c2c1e8a252509e0e50841973c13e
Sanjaya1717 <imaderikiwidiastanasanjaya@gmail.com>
2022-11-24 16:00:00 +0800

    tyring to deploy

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const reviewRoutes = require('./routes/reviews')
 const MongoDBStore = require('connect-mongodb-session')(session)
 
 // const dbUrl = process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 // 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -49,11 +49,15 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize())
+app.use(mongoSanitize({
+    replaceWith:'_'
+}))
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = new MongoDBStore({
     mongoUrl: dbUrl,
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     touchAfter: 24 * 60 * 60
 })
 
@@ -64,7 +68,7 @@ store.on('error', function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
